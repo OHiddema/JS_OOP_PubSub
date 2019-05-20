0b0db0cd7ac20b989adcead60ffa233dfd57e8ca
Oege Hiddema <oegehiddema@gmail.com>
2019-05-20 16:53:43 +0200

    add function to delete items from list

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 $(function() {
 
     $('#addPerson').on('click', function(){
-        let name = $('#name').val();
-        // alert (name);
-        appendStr = '<li><span>' + name + '</span><i class="del">X</i></li>'
-        $('#people').append(appendStr);
+
+        let ppl = document.getElementById('people')
+        let li = document.createElement('LI');
+        ppl.appendChild(li);
+
+        let sp = document.createElement('SPAN');
+        let node1 = document.createTextNode($('#name').val());
+        sp.appendChild(node1);
+
+        li.appendChild(sp);
+
+        let i = document.createElement('I');
+        i.setAttribute('class', 'del');
+        let node2 = document.createTextNode('X');
+        i.appendChild(node2);
+
+        li.appendChild(i);
+
+        i.onclick = function () {
+            this.parentNode.remove();
+        };
+    
+        // let name = $('#name').val();
+        // let appendStr = '<li><span>' + name + '</span><i class="del">X</i></li>'
+        // $('#people').append(appendStr);
+    });
+
+    $('.del').on('click',function(){
+        $(this).parent().remove();
     });
 
 });
